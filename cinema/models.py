6063d0cdbf820aa0ff4dc0df598326d48e314981
Nazar Serdyuk <nazarserdyuk122003@gmail.com>
2025-02-04 20:16:27 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
 
-from .utils import movie_image_path
+from cinema.utils import movie_image_path
 
 
 class CinemaHall(models.Model):
